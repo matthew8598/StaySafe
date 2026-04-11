dc560c213869608638d852b5218f7c7ccea2b788
Matúš Várfalvy <mates.var@gmail.com>
2026-04-11 14:38:04 +0200

    -added dotenv to backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "78.98.172.32",
-  port: 25566,
-  user: "user",
-  password: "12358Mn_-",
-  database: "staysafe",
+  host: process.env.HOST || "localhost",
+  port: process.env.PORT || 25566,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
 });
 
 export default db;
